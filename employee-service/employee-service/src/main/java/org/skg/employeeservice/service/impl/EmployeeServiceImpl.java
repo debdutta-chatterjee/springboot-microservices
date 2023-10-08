4f9c1b6645ef535c8834dc510be27cb7995357d5
Debdutta Chatterjee <debduttachatterjee09@gmail.com>
2023-10-08 13:47:09 +0530

    Rest sync microservice communication using Web template

--- employee-service/employee-service/src/main/java/org/skg/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/employee-service/src/main/java/org/skg/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -9,14 +9,15 @@ import org.skg.employeeservice.service.EmployeeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService
 {
     private EmployeeRepository employeeRepository;
-    private RestTemplate restTemplate;
-
+    //private RestTemplate restTemplate;
+    private WebClient webClient;
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto)
     {
@@ -44,12 +45,16 @@ public class EmployeeServiceImpl implements EmployeeService
     public APIResponseDto getEmployeeById(Long employeeId) {
         Employee employee = employeeRepository.findById(employeeId).get();
 
-        ResponseEntity<DepartmentDto> DepartmentDto = restTemplate.getForEntity(
-                "http://localhost:8080/api/departments/"+employee.getDepartmentCode(),
-                DepartmentDto.class
-                );
+//        ResponseEntity<DepartmentDto> DepartmentDto = restTemplate.getForEntity(
+//                "http://localhost:8080/api/departments/"+employee.getDepartmentCode(),
+//                DepartmentDto.class
+//                );
+//DepartmentDto departmentDto = DepartmentDto.getBody();
 
-        DepartmentDto departmentDto = DepartmentDto.getBody();
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/"+employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class).block();
 
         EmployeeDto employeeDto = new EmployeeDto(
                 employee.getId(),
